a5d3dbaf0d62937595b7ea9c3c448954c7fa8f24
Anup Kumar <anup.raj1989@gmail.com>
2022-03-08 12:55:23 +0530

    feat(Day-1): print intered text in console

--- src/App.js
+++ src/App.js
@@ -2,17 +2,13 @@ import React from "react";
 import "./App.css";
 
 const App = (props) => {
-  let counter = 0;
-
-  const increaseTheCounter = () => {
-    //This doesn't changes the value of counter in the View. But in console it changes
-    console.log("counter new Value is " + ++counter);
+  const handleInput = (e) => {
+    console.log(e.target.value);
   };
 
   return (
     <>
-      <h3>{counter}</h3>
-      <button onClick={increaseTheCounter}>Increment</button>
+      <input type="text" onInput={handleInput} />
     </>
   );
 };
