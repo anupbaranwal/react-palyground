ed65b90decd54d90eae41be90655f0aaacab74fd
Anup Kumar <anup.raj1989@gmail.com>
2022-03-08 12:36:52 +0530

    feat(Day-1): experimenting with some jsx features

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
+const Add = (a, b) => {
+  return a + b;
+};
+
+const currentTime = () => {
+  return 10;
+};
+
+const App = (props) => {
+  const hiThereJsx = <h3>Hi there</h3>;
+  const helloWorldJsx = <p>Hello There!</p>;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {hiThereJsx} {helloWorldJsx}
+      Add of 5 + 6 is: {Add(5, 6)}
+      {currentTime() < 12 ? <h3>Good Morning</h3> : <h3>Good Afternoon</h3>}
+    </>
   );
-}
-
+};
 export default App;
