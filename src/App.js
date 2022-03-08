1bc65ff7c4b13f6d924dec7f1f411c25ec0dbc9a
Anup Kumar <anup.raj1989@gmail.com>
2022-03-08 15:44:51 +0530

    feat(day-1): callback function from child to parent

--- src/App.js
+++ src/App.js
@@ -6,14 +6,17 @@ import Sample from "./Sample";
 const App = (props) => {
   const countries = ["India", "UK", "Russia", "US"];
   const name = "Anup";
+
+  const callBackParentFunction = () => {
+    console.log("I am good!! How about you?");
+  };
+
   return (
     <>
       <h2>Below is the child component</h2>
       <hr />
       <Sample countries={countries} />
-      <Greeting name={name}>
-        <span>This is a child of Greetings!!</span>
-      </Greeting>
+      <Greeting name={name} parentCallback={callBackParentFunction} />
     </>
   );
 };
--- src/Greeting.js
+++ src/Greeting.js
@@ -1,10 +1,15 @@
 const Greeting = (props) => {
+  const greetBack = () => {
+    if (!!props.parentCallback) {
+      props.parentCallback();
+    }
+  };
   return (
     <>
       <h2>Hello {props.name}!!</h2>
       <h2> How are you?</h2>
       <hr />
-      <h3>{props.children}</h3>
+      <button onClick={greetBack}>Greet Back!!</button>
     </>
   );
 };
