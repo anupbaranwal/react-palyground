a49f0058c312f57374f2f71476f2f7d8e84e0fff
Anup Kumar <anup.raj1989@gmail.com>
2022-03-09 10:35:17 +0530

    feat(day-2): lifecycle events handling by useEffect hook

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 
 const App = (props) => {
@@ -7,6 +7,18 @@ const App = (props) => {
   const incrementHandler = () => {
     setCounter(counter + 1);
   };
+
+  useEffect(() => {
+    console.log("everytime any component get render");
+  });
+
+  useEffect(() => {
+    console.log("only once during creation");
+  }, []);
+
+  useEffect(() => {
+    console.log("gets called when counter field is updated");
+  }, [counter]);
 
   return (
     <>
