f7a6dcee149a0ac372b97a26308d5bd04f6c3f94
Anup Kumar <anup.raj1989@gmail.com>
2022-03-09 15:35:51 +0530

    feat(day-2): using navbar for route navigation

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,9 +1,35 @@
-const NavBar = () => {
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const Navbar = (props) => {
   return (
-    <>
-      <h3>This is Nav Bar</h3>
-    </>
+    <React.Fragment>
+      <nav className="navbar navbar-expand navbar-dark bg-dark">
+        <a className="navbar-brand">
+          <img
+            src="logo512.png"
+            width="30"
+            height="30"
+            className="d-inline-block align-top mr-2"
+            alt=""
+          />
+          React Router Demo
+        </a>
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/" exact>
+              Home
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/about">
+              About Us
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </React.Fragment>
   );
 };
 
-export default NavBar;
+export default Navbar;
