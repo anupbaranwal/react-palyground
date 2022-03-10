49fc929b49b6916b1f2a67b7f6e3f83f39080351
Anup Kumar <anup.raj1989@gmail.com>
2022-03-10 11:00:28 +0530

    feat(day-3): showing api result in tabular form

--- src/Blog.js
+++ src/Blog.js
@@ -1,15 +1,15 @@
 import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 
 const Blog = (props) => {
   const { year, month, day } = useParams(); //  more convenient way
-  const [users, setUsers] = useState({});
+  const [users, setUsers] = useState([]);
 
   const loadBlogUsers = () => {
     axios.get("https://reqres.in/api/users?page=2").then((res) => {
       setUsers(() => {
-        return res.data;
+        return res.data.data;
       });
     });
   };
@@ -23,7 +23,32 @@ const Blog = (props) => {
         Blog published on {day}-{month}-{year}
       </p>
       <hr />
-      <pre>{JSON.stringify(users, null, 2)}</pre>
+      <table className="table table-bordered mt-4">
+        <thead>
+          <tr>
+            <td>Id</td>
+            <td>First Name</td>
+            <td>Last Name</td>
+            <td>Email</td>
+            <td>Avatar</td>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => {
+            return (
+              <tr key={user.id}>
+                <td className="fw-bold">{user.id}</td>
+                <td>{user.first_name}</td>
+                <td>{user.last_name}</td>
+                <td>{user.email}</td>
+                <td>
+                  <img src={user.avatar} alt="avatar" height="60" />
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </>
   );
 };
